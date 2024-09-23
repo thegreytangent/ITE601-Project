02e80b2022b2e934f59dc337c51c7fdcded20148
unknown <admin@CLATeacherPC.csabcomlab.edu.ph>
2024-09-23 10:40:33 +0800

    first commit

---
+++ Class/Template.php
@@ -0,0 +1,18 @@
+<?php
+
+class Template {
+
+    public function header() {
+        include 'templates/header.php';
+    }
+
+    public function footer() {
+        include 'templates/footer.php'; 
+    }
+
+    public function navbar() {
+        include 'templates/navbar.php';
+    }
+
+
+}
---
+++ templates/navbar.php
@@ -0,0 +1,19 @@
+<div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
+          <div class="container">
+            <div class="row align-items-center">
+            
+              <div class="col-lg order-lg-first">
+                <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
+                  <li class="nav-item">
+                    <a href="./index.html" class="nav-link active"><i class="fe fe-home"></i> List of Students</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="./index.html" class="nav-link active"><i class="fe fe-user"></i>Add New Student</a>
+                  </li>
+              
+                  
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
